a7c1271f199bb5fef7a1ea7d4f48f434c67718e9
jojolaperceuse <johan.perrissoud-piau@lycee-berthollet.com>
2025-05-22 09:54:28 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,34 @@
 document.getElementById("submitBtn").addEventListener("click", () => {
-  const password = document.getElementById("passwordInput").value;
+  const codepostale = document.getElementById("codepostaleInput").value;
+
+function verifierCodePostal() {
+    const codePostal = document.getElementById("codePostal").value;
+    const message = document.getElementById("message");
+
+    const regex = /^[0-9]{5}$/; // expression régulière pour un code postal à 5 chiffres
+
+    if (regex.test(codePostal)) {
+        message.textContent = "Code postal valide.";
+        message.style.color = "green";
+    } else {
+        message.textContent = "Code postal invalide. Veuillez entrer 5 chiffres.";
+        message.style.color = "red";
+    }
+}
+  // Envoi la requête POST au backend Python
+  fetch("/check-password", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("response").textContent = data.result;
+    })
+    .catch(() => {
+      document.getElementById("response").textContent = "Erreur serveur.";
+    });
+});
 
 
                                                       
